c9539a7c44354804fb53530091d0aee1e5169b7f
Adriano Boccardo Pochettino <adriano.boccardo@gmail.com>
2021-02-20 00:35:40 +0000

    14 - Firebase Setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+    production: true,
+    firebase: {
+        apiKey: "AIzaSyCaSBmTRocIQTZfSRSHdQ70QWehsMvHvEM",
+        authDomain: "angular-code-along-9d1f1.firebaseapp.com",
+        projectId: "angular-code-along-9d1f1",
+        storageBucket: "angular-code-along-9d1f1.appspot.com",
+        messagingSenderId: "867519257605",
+        appId: "1:867519257605:web:583928d15c2103463f506c"
+    }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyCaSBmTRocIQTZfSRSHdQ70QWehsMvHvEM",
+        authDomain: "angular-code-along-9d1f1.firebaseapp.com",
+        projectId: "angular-code-along-9d1f1",
+        storageBucket: "angular-code-along-9d1f1.appspot.com",
+        messagingSenderId: "867519257605",
+        appId: "1:867519257605:web:583928d15c2103463f506c"
+    }
 };
 
 /*
