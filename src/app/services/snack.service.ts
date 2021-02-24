10ee4a95d1a11eb72c8fb25c23e5c1c1677428da
Adriano Boccardo Pochettino <adriano.boccardo@gmail.com>
2021-02-24 23:19:57 +0000

    18 - Auth Guard

---
+++ src/app/services/snack.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Router } from "@angular/router";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { tap } from "rxjs/operators";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class SnackService {
+
+    constructor(private router: Router, private snackBar: MatSnackBar) { }
+
+    authError() {
+        this.snackBar.open('You must be logged in!', 'OK', {
+            duration: 5000
+        });
+
+        return this.snackBar._openedSnackBarRef
+            .onAction()
+            .pipe(
+                tap(_ =>
+                    this.router.navigate(['/login'])
+                )
+            )
+            .subscribe();
+    }
+}
